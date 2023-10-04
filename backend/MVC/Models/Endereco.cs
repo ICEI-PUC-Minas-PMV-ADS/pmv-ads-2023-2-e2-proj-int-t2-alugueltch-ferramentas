d5d887c73a420a7e580840d7074e80f01376c932
Vitor Oliveira <vitor_cos12@yahoo.com.br>
2023-10-03 21:57:58 -0300

    validacoes-campos-create-funcionario

--- backend/MVC/Models/Endereco.cs
+++ backend/MVC/Models/Endereco.cs
@@ -14,26 +14,26 @@ namespace MVC.Models
 
         public long Id { get; set; }
 
-        [Required(ErrorMessage = "O campo Logradouro é obrigatório.")]
+        [Required(ErrorMessage = "Insira o logradouro")]
         public string Logradouro { get; set; } = null!;
         public string? Complemento { get; set; }
 
-        [Required(ErrorMessage = "O campo Número é obrigatório.")]
+        [Required(ErrorMessage = "Insira o número")]
         public string Numero { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo bairro é obrigatório.")]
+        [Required(ErrorMessage = "Insira o bairro")]
         public string Bairro { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo cidade é obrigatório.")]
+        [Required(ErrorMessage = "Insira a cidade")]
         public string Cidade { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo Estado é obrigatório.")]
+        [Required(ErrorMessage = "Insira o estado")]
         public string Estado { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo País é obrigatório.")]
+        [Required(ErrorMessage = "Insira o país")]
         public string Pais { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo CEP é obrigatório.")]
+        [Required(ErrorMessage = "Insira o CEP")]
         public string Cep { get; set; } = null!;
 
         public virtual ICollection<Cliente> Clientes { get; set; }
--- backend/MVC/Models/Funcionario.cs
+++ backend/MVC/Models/Funcionario.cs
@@ -14,71 +14,61 @@ namespace MVC.Models
 
         public long Id { get; set; }
 
-        [Required(ErrorMessage = "O campo CPF é obrigatório.")]
+        [Required(ErrorMessage = "Insira o cpf")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "O cpf deve conter 11 dígitos.")]
         public string Cpf { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [Required(ErrorMessage = "Insira o nome")]
         public string Nome { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo Sexo é obrigatório.")]
+        [Required(ErrorMessage = "Insira o sexo")]
         public char Sexo { get; set; }
 
-        [Required(ErrorMessage = "O campo data de nascimento é obrigatório.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Insira o a data de nascimento")]
         public DateOnly DataNascimento { get; set; }
-        public string DataNascimentoFormatada()
-        {
-            return this.DataNascimento.ToString("dd/MM/yyyy");
-        }
 
-
-
-
-
-        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [Required(ErrorMessage = "Insira o e-mail")]
         public string Email { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo Telefone é obrigatório.")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "O telefone deve conter 11 dígitos.")]
+        [Required(ErrorMessage = "Insira o telefone")]
         public string Telefone { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo Funcional é obrigatório.")]
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "A matrícula deve conter o seguinte formato: FUNC+NNN.")]
+        [Required(ErrorMessage = "Insira a matrícula")]
 
         public string Funcional { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "O campo senha deve conter no mínimo 6 caracteres.")]
 
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Insira o senha")]
         public string Senha { get; set; } = null!;
 
-        [Required(ErrorMessage = "O campo Permissão é obrigatório.")]
+    
 
         public short PermissaoId { get; set; }
 
-        
-
-        [Required(ErrorMessage = "O campo Papel é obrigatório.")]
         public short PapelId { get; set; }
-
-      
-
 
         public long  EnderecoId { get; set; }
 
+
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         [Required(ErrorMessage = "O campo data de adimissão é obrigatório.")]
-
-
-        [Display(Name = "Data de Admissão")]
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateOnly DataAdmissao { get; set; }
-        public string DataAdmissaoFormatada()
-        {
-            return this.DataAdmissao.ToString("dd/MM/yyyy");
-        }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateOnly? DataDemissao { get; set; }
 
 
-        
-
-        [Required(ErrorMessage = "O campo Endereço é obrigatório.")]
+ 
         public virtual Endereco Endereco { get; set; }
 
         
