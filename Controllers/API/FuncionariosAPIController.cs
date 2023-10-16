83820fd8b2a2d77856fb5fa0d8ecffda1852e537
imrochamatheus <im.rochamatheus@gmail.com>
2023-10-15 22:08:15 -0300

    feat: adicionar endpoint para listagem de funcionários

---
+++ Controllers/API/FuncionariosAPIController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Models;
+using System.Linq;
+
+namespace MVC.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuncionariosAPIController : ControllerBase
+    {
+        private readonly atdbContext _context;
+
+        public FuncionariosAPIController(atdbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Funcionario>>> List(
+            [FromQuery] string? nome,
+            [FromQuery] string? funcional)
+        {
+
+            IQueryable<Funcionario> query = this._context
+                .Funcionarios;
+
+            if (!string.IsNullOrEmpty(nome)) 
+            {
+                query = this.FilterEmployeeByName(query, nome);
+            }
+
+            if (!string.IsNullOrEmpty(funcional))
+            {
+                query = FilterEmployeeByFuncional(query, funcional);
+            }
+
+            var employees = await query
+                .Include(funcionario => funcionario.Endereco)
+                .Include(funcionario => funcionario.Permissao)
+                .Select(funcionario => new
+                {
+                    funcionario.Id,
+                    funcionario.Nome,
+                    funcionario.Email,
+                    funcionario.Telefone,
+                    funcionario.Endereco,
+                    funcionario.Funcional,
+                    funcionario.Permissao,
+                    funcionario.DataAdmissao,
+                    funcionario.DataDemissao,
+                })
+                .ToListAsync();
+
+            return Ok(employees);
+        }
+
+        private IQueryable<Funcionario> FilterEmployeeByName(IQueryable<Funcionario> query, string nome)
+        {
+            query = query
+                .Where(funcionario =>
+                    funcionario.Nome
+                    .ToUpper()
+                    .Contains(nome.ToUpper()))
+                    .AsQueryable();
+
+            return query;
+        }
+
+        private IQueryable<Funcionario> FilterEmployeeByFuncional(IQueryable<Funcionario> query, string funcional)
+        {
+            query = query.Where(funcionario =>
+                    funcionario.Funcional
+                        .ToUpper()
+                        .Contains(funcional.ToUpper()));
+
+            return query;
+        }
+    }
+}
---
+++ utils/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace MVC.utils
+{
+    public static class Utils
+    {
+        public static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var character in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(character);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(character);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+    }
+}
